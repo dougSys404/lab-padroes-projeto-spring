ea70b6b069ecd223e662fa00d5bf807ca2424aed
Doug <douglas@macOs.local>
2025-05-14 23:58:47 -0300

    First commit

---
+++ src/main/java/br/com/dougsys404/gof/LabPadroesProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.dougsys404.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabPadroesProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPadroesProjetoSpringApplication.class, args);
+	}
+
+}
